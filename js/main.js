2344048c692017608e212d59187b46850295bf31
Jbuckley <buckleyjosh3@gmail.com>
2023-12-06 22:24:45 -0500

    first working product

--- js/main.js
+++ js/main.js
@@ -30,21 +30,12 @@ document.addEventListener('DOMContentLoaded', function () {
                 cell.classList.add('cell', 'hidden');
                 cell.dataset.row = row;
                 cell.dataset.col = col;
-    
-                // Add the cell to the board
+                cell.addEventListener('click', handleCellClick);
+                cell.addEventListener('contextmenu', handleCellClick);
                 board.appendChild(cell);
-    
-                // Check if the current cell contains a mine position
-                const isMine = minePositions.some(pos => pos.row === row && pos.col === col);
-    
-                // If it's a mine, add the 'mine' class
-                if (isMine) {
-                    cell.classList.add('mine');
-                }
             }
         }
     }
-    
 
     function placeMines() {
         const totalMines = 10;
@@ -66,8 +57,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 var col = parseInt(randomCell.dataset.col);
 
             } while (isMineAlreadyPlaced(row, col));
-
-            randomCell.classList.add('mine');
             minePositions.push({ row, col });
         }
     }
@@ -96,126 +85,85 @@ document.addEventListener('DOMContentLoaded', function () {
             }
 
             cell.dataset.adjacentMines = adjacentMineCount;
-
-            if (!cell.classList.contains('flagged')) {
-                cell.classList.add('hidden');
-            }
         });
     }
 
-    function renderBoard() {
-        const cells = document.querySelectorAll('.cell');
-    
-        cells.forEach(cell => {
-            const row = parseInt(cell.dataset.row);
-            const col = parseInt(cell.dataset.col);
-    
-            if (minePositions.some(pos => pos.row === row && pos.col === col)) {
-                // Handle mine cells separately
-                if (cell.classList.contains('revealed')) {
-                    // If mine cell is revealed, show a mine
-                    cell.innerHTML = '💣'; 
-                } else {
-                    cell.innerHTML = ''; // Hide content for hidden mine cells
-                }
-            } else if (cell.classList.contains('revealed')) {
-                // Already revealed cells
-                const adjacentMines = parseInt(cell.dataset.adjacentMines || 0);
-                if (adjacentMines > 0) {
-                    cell.textContent = adjacentMines;
-                } else {
-                    cell.innerHTML = ''; // Hide content for revealed cells without adjacent mines
-                }
-            } else {
-                // Hide content for hidden non-mine cells
-                cell.innerHTML = ''; //Ensuring content is hidden
-            }
-        });
-    }
-    
+    function revealEmptyCells(row, col) {
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                const neighborRow = row + i;
+                const neighborCol = col + j;
 
-    function handleCellClick(event) {
-        const clickedCell = event.target;
-        const row = parseInt(clickedCell.dataset.row);
-        const col = parseInt(clickedCell.dataset.col);
-
-        if (event.type === 'contextmenu') {
-            event.preventDefault();
-
-            if (!clickedCell.classList.contains('revealed')) {
-                clickedCell.classList.toggle('flagged');
-            }
-
-            return;
-        }
-
-        if (!clickedCell.classList.contains('flagged') && !clickedCell.classList.contains('revealed')) {
-            if (minePositions.some(pos => pos.row === row && pos.col === col)) {
-                // Game over: Clicked on a mine
-                alert('Game Over! You clicked on a mine.');
-            } else {
-                revealCell(row, col);
-                renderBoard(); // Call renderBoard after revealing cells
-            }
-        }
-    }
-
-    function revealCell(row, col) {
-        const cell = document.querySelector(`.cell[data-row="${row}"][data-col="${col}"]`);
-    
-        if (!cell || cell.classList.contains('revealed') || cell.classList.contains('flagged')) {
-            return;
-        }
-    
-        cell.classList.add('revealed');
-    
-        if (minePositions.some(pos => pos.row === row && pos.col === col)) {
-            // Revealed a mine
-            // Handle game over logic here
-            cell.innerHTML = '💣'; // Show mine when revealed
-        } else {
-            const adjacentMines = parseInt(cell.dataset.adjacentMines || 0);
-    
-            if (adjacentMines === 0) {
-                // Add neighboring cells to the list of cells to check
-                for (let i = -1; i <= 1; i++) {
-                    for (let j = -1; j <= 1; j++) {
-                        const neighborRow = row + i;
-                        const neighborCol = col + j;
-                        if (
-                            neighborRow >= 0 &&
-                            neighborRow < 8 &&
-                            neighborCol >= 0 &&
-                            neighborCol < 8
-                        ) {
-                            revealCell(neighborRow, neighborCol);
+                if (neighborRow >= 0 && neighborRow < 8 && neighborCol >= 0 && neighborCol < 8) {
+                    const cell = document.querySelector(`.cell[data-row="${neighborRow}"][data-col="${neighborCol}"]`);
+                    if (!cell.classList.contains('revealed') && !cell.classList.contains('flagged')) {
+                        cell.classList.add('revealed');
+                        const adjacentMines = parseInt(cell.dataset.adjacentMines || 0);
+                        if (adjacentMines === 0) {
+                            revealEmptyCells(neighborRow, neighborCol);
                         }
                     }
                 }
             }
         }
-        renderBoard(); // Call renderBoard after revealing cells
     }
 
-    // Remove the contextmenu event listener from individual cells
-    // It's already added to the board itself
-    // const cells = document.querySelectorAll('.cell');
-    // cells.forEach(cell => {
-    //     cell.addEventListener('contextmenu', handleCellClick);
-    // });
+    function renderBoard() {
+        const cells = document.querySelectorAll('.cell');
 
-    // Add right-click event listener to prevent the context menu
-    board.addEventListener('contextmenu', function (event) {
+        cells.forEach(cell => {
+            const row = parseInt(cell.dataset.row);
+            const col = parseInt(cell.dataset.col);
+
+            if (minePositions.some(pos => pos.row === row && pos.col === col)) {
+                if (cell.classList.contains('revealed')) {
+                    cell.innerHTML = '💣';
+                } else {
+                    cell.innerHTML = '';
+                }
+            } else if (cell.classList.contains('revealed')) {
+                const adjacentMines = parseInt(cell.dataset.adjacentMines || 0);
+                if (adjacentMines > 0) {
+                    cell.textContent = adjacentMines;
+                } else {
+                    cell.innerHTML = '';
+                }
+            } else {
+                cell.innerHTML = '';
+            }
+        });
+    }
+
+    function handleCellClick(event) {
         event.preventDefault();
-    });
+        const clickedCell = event.target;
+        const row = parseInt(clickedCell.dataset.row);
+        const col = parseInt(clickedCell.dataset.col);
 
-    board.addEventListener('click', handleCellClick);
+        if (event.type === 'contextmenu') {
+            if (!clickedCell.classList.contains('revealed')) {
+                clickedCell.classList.toggle('flagged');
+            }
+        } else {
+            if (!clickedCell.classList.contains('flagged') && !clickedCell.classList.contains('revealed')) {
+                if (minePositions.some(pos => pos.row === row && pos.col === col)) {
+                    // Reveal all mine cells on the board
+                    minePositions.forEach(pos => {
+                        const mineCell = document.querySelector(`.cell[data-row="${pos.row}"][data-col="${pos.col}"]`);
+                        mineCell.classList.add('revealed');
+                        mineCell.innerHTML = '💣';
+                    });
 
-    function checkWin() {
-        const revealedCells = document.querySelectorAll('.revealed');
-        const totalCells = document.querySelectorAll('.cell').length;
-        const totalMines = minePositions.length;
-
-        return revealedCells.length === totalCells - totalMines;
+                    // Trigger game over alert
+                    alert('Game Over! You clicked on a mine.');
+                } else {
+                    clickedCell.classList.add('revealed');
+                    if (parseInt(clickedCell.dataset.adjacentMines || 0) === 0) {
+                        revealEmptyCells(row, col);
+                    }
+                    renderBoard();
+                }
+            }
+        }
     }
 });
